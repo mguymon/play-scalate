49d6e8e0f0b767fc2a4f1e71146e5e7ca44732ee
Michael Phan-Ba <michael@mikepb.com>
2011-03-16 23:54:23 -0700

    Update test Application

--- samples-and-tests/just-test-cases/app/controllers/Application.scala
+++ samples-and-tests/just-test-cases/app/controllers/Application.scala
@@ -10,7 +10,7 @@ object Application extends ScalateController{
       user2.save()
       user3.save()
       val userlist = List(user2,user3)
-      render(user2,name,userlist)
+      Template(user2,name,userlist)
     }
 }
 
